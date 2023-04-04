dc41a85850160d811ca4eaee8748a83473215b5c
Dave Yonkers <dyonkers@prattmiller.com>
2023-04-04 12:05:51 -0400

    Adjust tolerances

--- server.py
+++ server.py
@@ -9,21 +9,24 @@ from flask_cors import CORS, cross_origin
 # global parameters
 width = 540
 height = 360
-keyPoints = [0,1,4,5,9,13,17,8,12,16,20]
+keyPoints = [0, 1, 4, 5, 9, 13, 17, 8, 12, 16, 20]
 allPoints = list(range(0, 21))
-thumbPoints = [0,1,2,3,4]
+thumbPoints = [0, 1, 2, 3, 4]
 tol = 1500
 
 # basically, some gestures were a lot harder to pass than others. This allows variability in acceptance prob
 # fingers tolerance
-tolerances_world = {"a": 0.6, "b": 0.70, "f": 0.5}
-tolerances_world_fingers = {"a":[0.08,0.1,0.1,0.1,0.1], "b":[0.17,0.1,0.1,0.1,0.1], "f":[0.1,0.1,0.1,0.1,0.1]}
+tolerances_world = {'a': 0.6, 'b': 0.55, 'c': 0.8, 'd': 0.40, 'e': 0.5, 'f': 0.55, 'g': 0.8, 'h': 0.92, 'i': 0.75, 'k': 0.6, 'l': 0.5,
+                    'm': 0.6, 'n': 0.6, 'o': 0.5, 'p': 1.5, 'q': 1, 'r': 0.5, 's': 0.5, 't': 0.85, 'u': 0.85, 'v': 0.95, 'w': 0.75, 'x': 0.5, 'y': 0.9}
+tolerances_world_fingers = {"a": [0.08, 0.1, 0.1, 0.1, 0.1], "b": [
+    0.17, 0.1, 0.1, 0.1, 0.1], "f": [0.1, 0.1, 0.1, 0.1, 0.1]}
 
 # stored gestures path
 current_dir = os.path.dirname(os.path.realpath('__file__'))
 rel_path = "gesturedatas" + os.sep + "gesture_info_alphabet_number_righthand.txt"
 abs_file_path = os.path.join(current_dir, rel_path)
-rel_path_world = "gesturedatas_world_coordinates" + os.sep + "gesture_info_alphabet_number_righthand.txt"
+rel_path_world = "gesturedatas_world_coordinates" + \
+    os.sep + "gesture_info_alphabet_number_righthand.txt"
 abs_file_path_world = os.path.join(current_dir, rel_path_world)
 
 
@@ -40,17 +43,20 @@ def jsonTohanddata(jsonData):
     Handdata = []
     # go through landmarks in the first hand
     for landmark in jsonData["multiHandLandmarks"][0]:
-        Handdata.append((int(landmark["x"]*width),int(landmark["y"]*height)))
+        Handdata.append((int(landmark["x"]*width), int(landmark["y"]*height)))
 
     Handdata_world = []
     # go through landmarks, but save the world coordinate data
     for landmark in jsonData["multiHandWorldLandmarks"][0]:
-        Handdata_world.append((float(landmark["x"]), float(landmark["y"]), float(landmark["z"])))
+        Handdata_world.append(
+            (float(landmark["x"]), float(landmark["y"]), float(landmark["z"])))
         pass
 
     return gestureName, Handdata, Handdata_world
 
 # read stored gesture info from file
+
+
 def readGestInfo(filepath):
     gesture_dict = {}
     gesture_data = []
@@ -67,30 +73,36 @@ def readGestInfo(filepath):
                 gesture_name_temp = pos
                 gesture_data = []
                 continue
-            
+
             pos_tuple = tuple(map(float, pos.split(' ')))
             gesture_data.append(pos_tuple)
 
         else:
             gesture_dict[gesture_name_temp] = gesture_data
     return gesture_dict
+
+
 GESTURES_DICT = readGestInfo(abs_file_path)
 GESTURES_DICT_WORLD = readGestInfo(abs_file_path_world)
 
-#Euclidean distance matrix functions
+# Euclidean distance matrix functions
+
+
 def getDistancesMatrix(handData):
     numjoints = len(handData)
-    distMatrix = np.zeros([numjoints, numjoints],dtype='float')
+    distMatrix = np.zeros([numjoints, numjoints], dtype='float')
     handData_array = np.array(handData)
     handmatrix = np.repeat(handData_array[np.newaxis, :, :], numjoints, axis=0)
     handmatrixT = np.transpose(handmatrix, (1, 0, 2))
-    distMatrix = np.linalg.norm(handmatrix - handmatrixT, axis = 2)
+    distMatrix = np.linalg.norm(handmatrix - handmatrixT, axis=2)
 
     return distMatrix
+
 
 def getFingerdata(handData, fingerPoints):
     fingerdata = [handData[i] for i in fingerPoints]
     return fingerdata
+
 
 def getAbsoluteAngle(point1, point2, fixedpoint):
     point1_ar = np.asarray(point1)
@@ -99,9 +111,11 @@ def getAbsoluteAngle(point1, point2, fixedpoint):
     line1 = point1_ar - fixedpoint_ar
     line2 = point2_ar - fixedpoint_ar
 
-    cosine_angle = np.dot(line1, line2) / (np.linalg.norm(line1) * np.linalg.norm(line2))
+    cosine_angle = np.dot(line1, line2) / \
+        (np.linalg.norm(line1) * np.linalg.norm(line2))
     angle = np.arccos(cosine_angle)
     return angle
+
 
 def getJointsAngles(fingerdata):
     # calculate angles in terms of the fixed point(the joint angle we need)
@@ -110,14 +124,16 @@ def getJointsAngles(fingerdata):
     angle3 = getAbsoluteAngle(fingerdata[2], fingerdata[4], fingerdata[3])
     # print([angle1, angle2, angle3])
     return np.array([angle1, angle2, angle3])
- 
+
+
 def findError(gestureMatrix, unknownMatrix, keyPoints):
-    error=0
+    error = 0
     points = np.array(keyPoints)
     errorMatrix = np.absolute(gestureMatrix - unknownMatrix)
     indices = np.ix_(points, points)
     error = errorMatrix[indices].sum()
     return error
+
 
 def verifyGesture(unknownGestureData, dictGesture, keyPoints, gestName, tol):
     unknownGesture = getDistancesMatrix(unknownGestureData)
@@ -127,7 +143,8 @@ def verifyGesture(unknownGestureData, dictGesture, keyPoints, gestName, tol):
 
     response = {}
     accepted = error < tol
-    response["accepted"] = str(accepted)  # boolean not serializable? how silly!
+    # boolean not serializable? how silly!
+    response["accepted"] = str(accepted)
     response["error"] = error
     response["tolerance"] = tol
     return response
@@ -135,24 +152,25 @@ def verifyGesture(unknownGestureData, dictGesture, keyPoints, gestName, tol):
 
 def verifyGestureWorld(unknownGestureData, dictGesture, keyPoints, gestName):
     knownGestureData = dictGesture[gestName]
-    unknownGesture = np.matrix(unknownGestureData)
-    knownGesture = np.matrix(knownGestureData)
+    # unknownGesture = np.matrix(unknownGestureData)
+    # knownGesture = np.matrix(knownGestureData)
 
     unknownGestureDistMatrix = getDistancesMatrix(unknownGestureData)
     knownGestureDistMatrix = getDistancesMatrix(knownGestureData)
-    total_error = findError(unknownGestureDistMatrix, knownGestureDistMatrix, keyPoints)
+    total_error = findError(unknownGestureDistMatrix,
+                            knownGestureDistMatrix, keyPoints)
 
     # calculate dist matrix for all landmarks in thumb
-    thumb_error = findError(unknownGestureDistMatrix, knownGestureDistMatrix, thumbPoints)
+    # thumb_error = findError(unknownGestureDistMatrix,
+    #                         knownGestureDistMatrix, thumbPoints)
 
     # calculate the anlges of joint of thumb
-    unknownFingerData = getFingerdata(unknownGestureData, thumbPoints)
-    knownFingerData = getFingerdata(knownGestureData, thumbPoints)
-    unknownFingerAngle = getJointsAngles(unknownFingerData)
-    knownFingerAngle = getJointsAngles(knownFingerData)
-    thumb_error_angle = np.max(np.abs(unknownFingerAngle - knownFingerAngle))
+    # unknownFingerData = getFingerdata(unknownGestureData, thumbPoints)
+    # knownFingerData = getFingerdata(knownGestureData, thumbPoints)
+    # unknownFingerAngle = getJointsAngles(unknownFingerData)
+    # knownFingerAngle = getJointsAngles(knownFingerData)
+    # thumb_error_angle = np.max(np.abs(unknownFingerAngle - knownFingerAngle))
     # print(thumb_error_angle)
-
 
     # find euclidean distance between points in the hand (which are rows). Units are in meters.
     # gesture_distance_deltas = knownGesture - unknownGesture
@@ -164,11 +182,12 @@ def verifyGestureWorld(unknownGestureData, dictGesture, keyPoints, gestName):
     # average_error = np.sum(euclidean_distance)
 
     tolerance = tolerances_world[gestName]
-    tolerance_fingers = tolerances_world_fingers[gestName][0]
+    # tolerance_fingers = tolerances_world_fingers[gestName][0]
 
     response = {}
     accepted = total_error < tolerance
-    response["accepted"] = str(accepted)  # boolean not serializable? how silly!
+    # boolean not serializable? how silly!
+    response["accepted"] = str(accepted)
     response["error"] = total_error
     # response["error_avg"] = average_error
     response["tolerance"] = tolerance
@@ -205,16 +224,19 @@ def create_app():
 
             # ~~~ Future AI function call here ~~~
             # ai_result = ai_function(data)
-            correctGestName, unknownhanddata, unknownhanddata_world = jsonTohanddata(data)
-            ai_result = verifyGesture(unknownhanddata, GESTURES_DICT, keyPoints, correctGestName, tol)
-            ai_result_world = verifyGestureWorld(unknownhanddata_world, GESTURES_DICT_WORLD, keyPoints, correctGestName)
+            correctGestName, unknownhanddata, unknownhanddata_world = jsonTohanddata(
+                data)
+            # ai_result = verifyGesture(
+            #     unknownhanddata, GESTURES_DICT, keyPoints, correctGestName, tol)
+            ai_result_world = verifyGestureWorld(
+                unknownhanddata_world, GESTURES_DICT_WORLD, keyPoints, correctGestName)
 
-            return jsonify(ai_result_world)  # send back the result to the frontend
+            # send back the result to the frontend
+            return jsonify(ai_result_world)
 
         # neither get nor post
         else:
             return "Method not allowed", 405
-
 
     return app
 
@@ -226,4 +248,3 @@ def create_app():
 #         z = landmark["z"]
 #         HandData.append((int(x*width),int(y*height))
 #     return gestureName, HandData
-
