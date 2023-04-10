aecdda439f9fb53882c513433db796508f5ec3d4
Dave Yonkers <daveyonks@gmail.com>
2023-04-10 08:50:16 -0400

    switch from waitress to gunicorn, since gunicorn has SSL support without a reverse-proxy

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd /home/biff/Repositories/HAI-backend
 source venv/bin/activate
-python server.py
+gunicorn --certfile='/etc/letsencrypt/live/api.daveyonkers.com/cert.pem' --keyfile='/etc/letsencrypt/live/api.daveyonkers.com/privkey.pem' --bind 0.0.0.0:443 'server:create_app()'
